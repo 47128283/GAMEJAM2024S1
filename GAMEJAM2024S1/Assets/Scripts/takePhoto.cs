52aa161cc2da1294a45d5292cd1dc34d5313b372
Ivan Machamer <112920308+47128283@users.noreply.github.com>
2024-09-20 11:33:46 +1000

    boo
    boo

--- GAMEJAM2024S1/Assets/Scripts/takePhoto.cs
+++ GAMEJAM2024S1/Assets/Scripts/takePhoto.cs
@@ -11,46 +11,59 @@ public class takePhoto : MonoBehaviour
     private Boolean toggleCamera;
     public GameObject[] targets;
     public RenderTexture[] plaques;
+	public RenderTexture source;
+	public RenderTexture kill;
     // Start is called before the first frame update
     void Start()
     {
         toggleCamera = false;
+		for(int i = 0;i<plaques.Length;i++)
+		{
+			Graphics.CopyTexture(kill,plaques[i]);
+		}
     }
 
     // Update is called once per frame
     
 
-private void Update()
-{
+	private void Update()
+	{
     
-    if (Input.GetKeyDown(toggleButton)) 
-    {
-        toggleCamera = !toggleCamera;
-    }
-    
-    Cast(targets[0], plaques[0]);
-}
+		if (Input.GetKeyDown(toggleButton)) 
+		{
+			toggleCamera = !toggleCamera;
+		}
+		if(targets.Length == plaques.Length)
+		{
+			for(int i = 0;i<targets.Length;i++)
+			{
+				Cast(targets[i], plaques[i]);
+			}
+		}
+		
+	}
 
-private void Capture(RenderTexture boo) 
-{
-    //ScreenCapture.CaptureScreenshot(Application.dataPath + "/Resources/" + name + ".png");
-    //UnityEditor.AssetDatabase.Refresh();
-    ScreenCapture.CaptureScreenshotIntoRenderTexture(boo);
-    Debug.Log("A screenshot was taken!");
-}
+	private void Capture(RenderTexture boo) 
+	{
+		//ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + System.DateTime.Now.ToString("yy-mm-dd (HH-mm-ss)") + ".png");
+		//UnityEditor.AssetDatabase.Refresh();
+		//ScreenCapture.CaptureScreenshotIntoRenderTexture(boo);
+		Graphics.CopyTexture(source,boo);
+		//Debug.Log("A screenshot was taken!");
+	}
 
-private void Cast(GameObject target, RenderTexture plaque) 
-{
-    Vector3 dir = (target.transform.position - transform.position).normalized;
-    float dot = Vector3.Dot(dir, transform.forward);
-    if(dot>0.9) 
-    {
-        if (Input.GetKeyDown(screenShotButton)&&toggleCamera)
-    {
-       //Capture(target.name);
-       //plaque.GetComponent<Renderer>().material.mainTexture = Resources.Load(Application.dataPath + "/Resources/" + target.name + ".png") as Texture;
-       Capture(plaque);
-    }
-    }
-}
+	private void Cast(GameObject target, RenderTexture plaque) 
+	{
+		Vector3 dir = (target.transform.position - transform.position).normalized;
+		float dot = Vector3.Dot(dir, transform.forward);
+		if(dot>0.9) 
+		{
+			if (Input.GetKeyDown(screenShotButton)&&toggleCamera)
+			{
+				//Capture(target.name);
+				//plaque.GetComponent<Renderer>().material.mainTexture = Resources.Load(Application.dataPath + "/Resources/" + target.name + ".png") as Texture;
+				Capture(plaque);
+			}
+		}
+	}
 }
